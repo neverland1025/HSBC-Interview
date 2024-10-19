ebb9d27cfeadea067ec2fbff6960c9aa6ced76c8
Fangyu Gu <38585177+neverland1025@users.noreply.github.com>
2024-10-19 19:31:48 +0800

    Add files via upload

--- interview/quiz.py
+++ interview/quiz.py
@@ -68,19 +68,3 @@ def solve_sudoku(board):
     solve()
     return board
 
-
-example_board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
-
-solved_board = solve_sudoku(example_board)
-for row in solved_board:
-    print(row)
